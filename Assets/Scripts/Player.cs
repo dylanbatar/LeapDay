a593a7ef838561c7e5a60fa4bc71b85dd80d8795
dylanbatar <dylanbatar2@gmail.com>
2021-10-10 21:37:07 -0500

    Basic move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D body;
+    SpriteRenderer sprite;
+    float vertical;
+    bool direction = true;
+    public float runSpeed = 10.0f;
+    public float jumpForce = 20.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2((direction ? 1 : -1) * runSpeed, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump(jumpForce);
+        }
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Walls")
+        {
+            sprite.flipX = direction;
+            direction = !direction;
+        }
+    }
+
+    public void Jump(float inpulse = 10.0f)
+    {
+        Debug.Log("space");
+        body.AddForce(new Vector2(0, inpulse), ForceMode2D.Impulse);
+    }
+}
