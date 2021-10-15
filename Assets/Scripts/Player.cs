f572432751f95db14621bfcc6ed41be7f4bbbbbe
Oscar H. Guzman <oguzman@condorlabs.io>
2021-10-15 16:55:19 -0500

    el frito del okachon

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,40 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D body;
-    SpriteRenderer sprite;
-    float vertical;
-    bool direction = true;
-    public float runSpeed = 10.0f;
-    public float jumpForce = 20.0f;
+  Rigidbody2D body;
+  SpriteRenderer sprite;
+  float vertical;
+  bool direction = true;
+  public float runSpeed = 10.0f;
+  public float jumpForce = 20.0f;
 
-    void Start()
+  public bool touchingFloor = true;
+
+  void Start()
+  {
+    body = GetComponent<Rigidbody2D>();
+    sprite = GetComponent<SpriteRenderer>();
+
+  }
+
+  private void FixedUpdate()
+  {
+    body.velocity = new Vector2((direction ? 1 : -1) * runSpeed, 0);
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        body = GetComponent<Rigidbody2D>();
-        sprite = GetComponent<SpriteRenderer>();
+      touchingFloor = false;
+      Jump(jumpForce);
     }
 
-    private void FixedUpdate()
+  }
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.tag == "Walls")
     {
-        body.velocity = new Vector2((direction ? 1 : -1) * runSpeed, 0);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Jump(jumpForce);
-        }
+      sprite.flipX = direction;
+      direction = !direction;
+    }
+  }
 
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Walls")
-        {
-            sprite.flipX = direction;
-            direction = !direction;
-        }
-    }
-
-    public void Jump(float inpulse = 10.0f)
-    {
-        Debug.Log("space");
-        body.AddForce(new Vector2(0, inpulse), ForceMode2D.Impulse);
-    }
+  public void Jump(float inpulse = 10.0f)
+  {
+    Debug.Log("space");
+    body.AddForce(new Vector2(0, inpulse), ForceMode2D.Impulse);
+    touchingFloor = true;
+  }
 }
